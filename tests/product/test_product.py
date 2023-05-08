a96381ad31da4786df351094f32339aa83ff8fc6
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-08 15:25:01 -0300

    test: req 1 - class product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.factories.product_factory import ProductFactory
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product_factory = ProductFactory()
+    product = Product(
+        product_factory.id,
+        product_factory.nome_do_produto,
+        product_factory.nome_da_empresa,
+        product_factory.data_de_fabricacao,
+        product_factory.data_de_validade,
+        product_factory.numero_de_serie,
+        product_factory.instrucoes_de_armazenamento,
+    )
+
+    assert product.id == product_factory.id
+    assert product.nome_do_produto == product_factory.nome_do_produto
+    assert product.nome_da_empresa == product_factory.nome_da_empresa
+    assert product.data_de_fabricacao == str(
+        product_factory.data_de_fabricacao
+    )
+    assert product.data_de_validade == str(product_factory.data_de_validade)
+    assert product.numero_de_serie == product_factory.numero_de_serie
+    assert (
+        product.instrucoes_de_armazenamento
+        == product_factory.instrucoes_de_armazenamento
+    )
