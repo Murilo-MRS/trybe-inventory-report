301303ad5d6d12cccf74b48dd4db6e71762e9074
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-08 16:59:21 -0300

    feat: req 3 - add class CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data_list: list):
+        response = SimpleReport.generate(data_list)
+
+        most_productive_company = Counter(
+            [product["nome_da_empresa"] for product in data_list]
+        )
+
+        response += "\nProdutos estocados por empresa:\n"
+        for company, quantity in most_productive_company.items():
+            print(company, quantity)
+            response += "- {}: {}\n".format(company, quantity)
+
+        return response
