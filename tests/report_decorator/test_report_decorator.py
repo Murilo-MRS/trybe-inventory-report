23976fa8fbc69cbbd0090628aab83fb10cd13ec2
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-09 15:04:16 -0300

    test: :white_check_mark: req 9  - report decorator

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,26 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from tests.factories.product_factory import ProductFactory
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    mock = []
+
+    new_product = ProductFactory()
+    mock.append(vars(new_product))
+
+    colored_report = ColoredReport(SimpleReport)
+    result = colored_report.generate(mock)
+
+    assert (
+        "\033[32mData de fabricação mais antiga:\033[0m "
+        f"\033[36m{new_product.data_de_fabricacao}\033[0m" in result
+    )
+    assert (
+        "\033[32mData de validade mais próxima:\033[0m "
+        f"\033[36m{str(new_product.data_de_validade)}\033[0m" in result
+    )
+    assert (
+        "\033[32mEmpresa com mais produtos:\033[0m "
+        f"\033[31m{new_product.nome_da_empresa}\033[0m" in result
+    )
