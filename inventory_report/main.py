7aada474a280a2229f826d9739f07a832b6b2a45
Murilo-MRS <murilosantana1993@gmail.com>
2023-05-09 21:11:28 -0300

    feat: :sparkles: req 11  - add gerar e rolatorio pelo comando inventory_report <arg1> <arg2>

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,30 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
+def importer_factory(file_path):
+    file_extension = file_path.split(".")[-1]
+    if file_extension not in ["csv", "json", "xml"]:
+        raise ValueError("Arquivo inválido")
+
+    if file_extension == "csv":
+        return CsvImporter()
+    elif file_extension == "json":
+        return JsonImporter()
+    elif file_extension == "xml":
+        return XmlImporter()
+
+
 def main():
-    pass
+    try:
+        file_path = sys.argv[1]
+        report_type = sys.argv[2]
+        importer = importer_factory(file_path)
+        inventory = InventoryRefactor(importer)
+        new_report = inventory.import_data(file_path, report_type)
+        sys.stdout.write(new_report)
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
